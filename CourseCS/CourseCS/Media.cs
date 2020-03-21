7adf22dae4bb8e171257d8e1178d0c05ce6fb5c5
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-03-21 18:45:14 +0100

    Herencia

---
+++ CourseCS/CourseCS/Media.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseCS
+{
+    class Media: IMedia
+    {
+
+        public void play() { 
+            Console.Write("Playing")
+        }
+    }
+}
